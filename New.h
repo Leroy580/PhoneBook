51aa836b23c5ae206d8b1e77d5abca516185f2b3
Leroy580 <zazkal222@gmail.com>
2024-11-19 03:51:30 +0100

    Create New.h

---
+++ New.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include "Person.h"
+void Sort(Person*& person, Hp hp); // Function to sort the Person array by surname
+void New(Person*& group, Hp& hp); // Function to add a new Person to the list

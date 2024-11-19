38a5c90e4876a903dc65227d48b2cc9244e677ef
Leroy580 <zazkal222@gmail.com>
2024-11-19 03:50:27 +0100

    Create Write.h

---
+++ Write.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "Person.h"
+// Function to write Person and Group data back to files
+void Write(Person* group, Grup* grup, Hp hp, const char* filename, const char* filename2);

ffe852dae0c8b3be62de0e4435d8662469638e68
Leroy580 <zazkal222@gmail.com>
2024-11-19 03:51:10 +0100

    Create Read.h

---
+++ Read.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include "Person.h"
+// Function to read data from files and populate Person and Grup arrays
+void Read(Person*& person, Grup*& grup, Hp& hp, const char* filename, const char* filename2);
